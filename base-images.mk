713497d4d39490c5bd255225d02b57c0a75f1d29
Wodby Robot <robot@wodby.com>
2026-09-24 07:03:44 +0000

    Update base image revision to r4

--- base-images.mk
+++ base-images.mk
@@ -4,7 +4,7 @@ BASE_IMAGE_REPOSITORY := wodby/php
 BASE_IMAGE_VERSION_SUFFIX :=
 
 BASE_IMAGE_DIGEST_8.2 := sha256:967b004edfe8be44ed7e31d84b23fb39f1b55996c2708129cbddbe599b5f2bfb
-BASE_IMAGE_DIGEST_8.2-r2 := sha256:a1113ddba370e9b347925b20387f1e7332f1c31574aea2725382f889f8f03c53
+BASE_IMAGE_DIGEST_8.2-r4 := sha256:c374f68216ff78ce766e93e02fd3faa90cb5c9ae5db5b12a747533ddfae5b4e0
 
 # Fail before building when a version or variant has no reviewed pin.
 BASE_IMAGE = $(BASE_IMAGE_REPOSITORY):$(BASE_IMAGE_TAG)@$(or $(BASE_IMAGE_DIGEST_$(BASE_IMAGE_TAG)),$(error No base image digest for $(BASE_IMAGE_REPOSITORY):$(BASE_IMAGE_TAG); update base-images.mk))
